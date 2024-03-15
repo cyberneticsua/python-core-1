03c37a8bb18c15f44f678d52bd5ba469a4a38de4
Davyd Kuzovenkov <davidkuzovenkov@gmail.com>
2024-03-15 13:47:50 +0200

    3 Task

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,3 +1,10 @@
 def fizz_buzz(param):
-  #Type your code here
-  return
+    if param % 3 == 0 and param % 5 != 0:
+        return "fizz"
+    elif param % 3 != 0 and param % 5 == 0:
+        return "buzz"
+    elif param % 3 == 0 and param % 5 == 0:
+        return "fizzbuzz"
+    else:
+        return param
+
