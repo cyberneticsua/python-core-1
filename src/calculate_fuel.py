ef41fabc4a29f061cefc566321ca5b3e03b13d13
Davyd Kuzovenkov <davidkuzovenkov@gmail.com>
2024-04-26 09:51:10 +0300

    4

--- src/calculate_fuel.py
+++ src/calculate_fuel.py
@@ -1,3 +1,6 @@
 def calculate_fuel(distance):
-  # Type your code
-  return
+  def calculate_fuel(distance):
+    fuel = distance * 10  # Обсяг палива - 10 разів більше відстані
+    if fuel < 100:  # Перевіряємо, чи не менше за 100 літрів
+        return 100
+    return fuel
