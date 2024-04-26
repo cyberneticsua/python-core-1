4ce760c9e750968d91312fec8a8566f9255eb152
Davyd Kuzovenkov <davidkuzovenkov@gmail.com>
2024-04-26 09:55:37 +0300

    10

--- src/sum_even_nums_in_range.py
+++ src/sum_even_nums_in_range.py
@@ -1,3 +1,6 @@
 def sum_even_nums_in_range(start, stop):
-  # Type your code
-  return
+  sum_of_evens = 0
+  for num in range(start, stop + 1):
+    if num % 2 == 0:
+      sum_of_evens = sum_of_evens + num
+  return sum_of_evens
